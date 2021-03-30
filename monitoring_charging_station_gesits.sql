42b9fedbeda1bd655b121bed136083692ce0d9cf
dodypramana <edp.dody@gmail.com>
2021-03-30 22:15:01 +0700

    file database

--- monitoring_charging_station_gesits.sql
+++ monitoring_charging_station_gesits.sql
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `tbl_charging_station`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tbl_charging_station` (
   `id_charging_station` int NOT NULL AUTO_INCREMENT,
-  `nomor_charging_station` varchar(100) DEFAULT NULL,
+  `nama_charger` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_charging_station`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `tbl_charging_station` (
 
 LOCK TABLES `tbl_charging_station` WRITE;
 /*!40000 ALTER TABLE `tbl_charging_station` DISABLE KEYS */;
+INSERT INTO `tbl_charging_station` VALUES (1,'Charging Station 1','2021-03-30 22:11:28');
 /*!40000 ALTER TABLE `tbl_charging_station` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,11 +50,16 @@ DROP TABLE IF EXISTS `tbl_monitoring`;
 CREATE TABLE `tbl_monitoring` (
   `id_monitoring` bigint NOT NULL AUTO_INCREMENT,
   `fk_charging_station` int DEFAULT NULL,
-  `waktu` datetime DEFAULT CURRENT_TIMESTAMP,
   `arus` double DEFAULT NULL,
   `tegangan` double DEFAULT NULL,
-  PRIMARY KEY (`id_monitoring`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `daya` double DEFAULT NULL,
+  `biaya` int DEFAULT NULL,
+  `lifetime` bigint DEFAULT NULL,
+  `waktu` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_monitoring`),
+  KEY `tbl_monitoring_FK` (`fk_charging_station`),
+  CONSTRAINT `tbl_monitoring_FK` FOREIGN KEY (`fk_charging_station`) REFERENCES `tbl_charging_station` (`id_charging_station`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +68,7 @@ CREATE TABLE `tbl_monitoring` (
 
 LOCK TABLES `tbl_monitoring` WRITE;
 /*!40000 ALTER TABLE `tbl_monitoring` DISABLE KEYS */;
-INSERT INTO `tbl_monitoring` VALUES (1,1,'2021-03-28 15:53:50',7.2,10);
+INSERT INTO `tbl_monitoring` VALUES (1,1,7,4.2,8,1000,14,'2021-03-30 22:12:41'),(3,1,7,4.2,8,1000,14,'2021-03-30 22:12:44');
 /*!40000 ALTER TABLE `tbl_monitoring` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-28 15:55:24
+-- Dump completed on 2021-03-30 22:13:25
