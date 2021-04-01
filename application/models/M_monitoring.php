63098bdeaef5e3e32cd21515f18e55d77e9b114a
dodypramana <edp.dody@gmail.com>
2021-04-01 09:17:17 +0700

    penambahan fungsi get data untuk header

--- application/models/M_monitoring.php
+++ application/models/M_monitoring.php
@@ -12,7 +12,7 @@ class M_monitoring extends CI_Model {
 	}
 
 	function get_by_id($id){
-		$this->db->select('*');
+		$this->db->select('m.waktu, m.arus, m.tegangan');
         $this->db->from('tbl_monitoring m');
         $this->db->join('tbl_charging_station c','m.fk_charging_station = c.id_charging_station');
         $this->db->where('c.id_charging_station',$id);
@@ -20,4 +20,16 @@ class M_monitoring extends CI_Model {
 
         return $this->db->get();
 	}
+
+    public function get_data_header($id)
+    {
+        $this->db->select('m.*, c.nama_charger');
+        $this->db->from('tbl_monitoring m');
+        $this->db->join('tbl_charging_station c','m.fk_charging_station = c.id_charging_station');
+        $this->db->where('c.id_charging_station',$id);
+        $this->db->order_by('m.id_monitoring','DESC');
+        $this->db->limit(1);
+
+        return $this->db->get();
+    }
 }
